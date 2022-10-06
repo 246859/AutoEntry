81af0c40bbfa1fb402cb95c33c7311609a3664bb
246859 <2633565580@qq.com>
2022-10-06 13:02:39 +0800

    增加交互式生成功能

--- src/index.js
+++ src/index.js
@@ -1,47 +1,77 @@
-const fs  = require('fs');
+const fs = require('fs');
+const rd = require('readline');
 const template = require('art-template');
 
+
+const rl = rd.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.setPrompt("AutoEntry>");
+let entryPath = undefined;
+let outputDir = undefined;
+
 //格式化json
-function formatJson(data){
-    return JSON.stringify(data,null,'\t');
+function formatJson(data) {
+    return JSON.stringify(data, null, '\t');
 }
 
-fs.readFile('./index.json','utf8',(err,data)=>{
-    let entryData = JSON.parse(data);
-    fs.readFile('./template/index.md','utf8',(err,data)=>{
-        //遍历词条数据文件
-        for (let typeKey in entryData){
-            let achiType = entryData[typeKey];
-            let details = entryData[typeKey].details;
-            //词条数组
-            let entryArr = [];
-            //填充数据
-            for (let entryKey in details){
-                entryArr.push({
-                    key:entryKey,
-                    msg:details[entryKey].msg,
-                    condition:details[entryKey].condition,
-                    enable:details[entryKey].enable,
-                    json:formatJson({[entryKey]:details[entryKey]})
-                })
-            }
+function main() {
+    if (!entryPath || !outputDir) {
+        console.log("错误的路径");
+        return;
+    }
 
-            achiType.details = {};//展示类型时不需要细节
+    fs.readFile(entryPath, 'utf8', (err, data) => {
+        let entryData = JSON.parse(data);
+        fs.readFile('./template/index.md', 'utf8', (err, data) => {
+            //遍历词条数据文件
+            for (let typeKey in entryData) {
+                let achiType = entryData[typeKey];
+                let details = entryData[typeKey].details;
+                //词条数组
+                let entryArr = [];
+                //填充数据
+                for (let entryKey in details) {
+                    entryArr.push({
+                        key: entryKey,
+                        msg: details[entryKey].msg,
+                        condition: details[entryKey].condition,
+                        enable: details[entryKey].enable,
+                        json: formatJson({[entryKey]: details[entryKey]})
+                    })
+                }
 
-            //将所有数据包装起来
-            const content = {
-                type:{
-                    key:typeKey,
-                    name:achiType.name,
-                    enable:achiType.enable,
-                    json:formatJson({[typeKey]:achiType})
-                },
-                entryArr
+                achiType.details = {};//展示类型时不需要细节
+
+                //将所有数据包装起来
+                const content = {
+                    type: {
+                        key: typeKey,
+                        name: achiType.name,
+                        enable: achiType.enable,
+                        json: formatJson({[typeKey]: achiType})
+                    },
+                    entryArr
+                }
+                const mdFileContent = template.render(data, content);
+                const path = `${outputDir}/${typeKey}.md`;
+                fs.writeFileSync(path, mdFileContent, 'utf8');
+                console.log(`路径:${path},文件生成成功,`);
             }
-            const mdFileContent = template.render(data,content);
-            const path = `./markdown/${typeKey}.md`;
-            fs.writeFileSync(path,mdFileContent,'utf8');
-            console.log(`路径:${path},文件生成成功,`);
-        }
+        });
+    });
+}
+
+
+rl.question("input your entry.json path:", (answer) => {
+    entryPath = answer;
+    rl.question("input your output path:", (answer) => {
+        outputDir = answer;
+        main();
     });
 });
+
+
+
